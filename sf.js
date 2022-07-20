f1e9cd76788ac61d3d6342fcd85d95b3d7710e0a
Fauzyah Hadirahma <97279376+fauzyahadirahma@users.noreply.github.com>
2022-07-20 22:49:13 +0700

    file js yang terhubung dengan file HTML

---
+++ sf.js
@@ -0,0 +1,12 @@
+function myFunction() {
+    document.getElementById("tekanGambar").style.color = "#FFC900";
+}
+
+function myFunction2() {
+    document.getElementById("tekanGambar2").style.color = "#FFC900";
+}
+
+function myFunction3() {
+    document.getElementById("tekanGambar3").style.color = "#FFC900";
+}
+
